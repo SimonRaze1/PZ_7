7bb63a2c81e342cd14ee6b54d72105aca6686c10
User <User@DESKTOP-AMGP8UK>
2023-12-20 21:52:30 +0500

    Добавьте файлы проекта.

---
+++ PZ_7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int size = 4; // задаем размерность матрицы
+        int[,] matrix = new int[size, size]
+        {
+            {1, -2, 3, 4},
+            {5, 6, -7, 8},
+            {-9, 10, 11, -12},
+            {13, 14, 15, 16}
+        };
+
+        int countPositive = 0;
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = i + 1; j < size; j++)
+            {
+                if (matrix[i, j] > 0) // проверяем, является ли элемент положительным
+                {
+                    countPositive++; // увеличиваем счетчик положительных элементов
+                }
+            }
+        }
+
+        Console.WriteLine("Количество положительных элементов, расположенных над главной диагональю: " + countPositive);
+    }
+}
